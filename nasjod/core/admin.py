4b33563526f98eded5b16db55501826c4cd8049d
Mohamed Guirat <guirat.meds@gmail.com>
2024-05-13 09:12:40 +0200

    (feat) add custom admin map template

--- nasjod/core/admin.py
+++ nasjod/core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib.gis import admin
 from .models import Address
 
+class AddressAdmin(admin.GISModelAdmin):
+    # Setting the default map template
+    # map_template = 'gis/admin/custom_osm.html'
+    pass
 
-admin.site.register(Address, admin.ModelAdmin)
+admin.site.register(Address, AddressAdmin)
