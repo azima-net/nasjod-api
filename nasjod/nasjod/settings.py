ec2d57f1b12f6caa05d5d2fdac0c2783e099708a
Mohamed Guirat <guirat.meds@gmail.com>
2024-06-18 00:02:37 +0200

    (security) add CORS

--- nasjod/nasjod/settings.py
+++ nasjod/nasjod/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_filters",
+    'corsheaders',
     "rest_framework",
     "drf_spectacular",
     "django.contrib.gis",
@@ -52,11 +53,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "nasjod.urls"
