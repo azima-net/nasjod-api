1da04ea5e81f4ed5ba9b6e270344666a25af695a
Mohamed Guirat <guirat.meds@gmail.com>
2024-06-20 16:15:11 +0200

    (fix) telephone unicity

--- nasjod/user/models.py
+++ nasjod/user/models.py
@@ -67,7 +67,7 @@ class User(AbstractBaseUser, PermissionsMixin, GDPR_compliance):
     birth_date = models.DateField(null=True)
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True, unique=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
