52947c35a47e573f76b1b05b8b4f281b5361e160
Mohamed Guirat <guirat.meds@gmail.com>
2024-06-27 09:16:08 +0200

    cors settings

--- nasjod/nasjod/settings.py
+++ nasjod/nasjod/settings.py
@@ -67,8 +67,11 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# Allow all origins
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://front:3000",
+    "http://localhost:3000",
+    "http://nasjod.com:3000",
+]
 
 ROOT_URLCONF = "nasjod.urls"
 
