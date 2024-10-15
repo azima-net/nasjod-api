84a73ee609ad10e35ad41f73b77224150db040d7
mguirat <guirat.meds@gmail.com>
2024-10-15 11:57:55 +0200

    make inifinte throuttling by default

--- nasjod/nasjod/settings.py
+++ nasjod/nasjod/settings.py
@@ -195,8 +195,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
     ),
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
+        'anon': os.getenv('THROTTLE_ANON', 'infinite'),
+        'user': os.getenv('THROTTLE_USER', 'infinite'),
     },
 
     # Renderer settings
